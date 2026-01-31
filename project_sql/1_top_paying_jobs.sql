e092cc9be23fc44d4abe31818340bbb815b808d0
m0niquee <moniquenguyen425@gmail.com>
2026-01-31 11:14:02 -0800

    Update: update query for remote and CA jobs

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -3,7 +3,7 @@ QUESTION: What are the top-paying data analyst jobs?
 - Identify the top 50 highest-paying Data Analyst roles that are available remotely
 - Focuses on job postings with specified salaries (remove nulls)
 - Why? Highlight the top-paying opportunities for Data Analysis, offering insights into employment option and location flexibility
-- Since I live in CA if I were to work on-site I wouldn't mind moving somwhere within CA
+- Wanted to specify specifically jobs that were either remote or in CA
 */
 
 SELECT
@@ -18,7 +18,7 @@ FROM job_postings_fact AS job_postings
 LEFT JOIN company_dim AS company ON company.company_id = job_postings.company_id
 WHERE
     job_title_short = 'Data Analyst' AND
-    (job_location LIKE 'Anywhere' OR job_location LIKE '%CA%') AND
+    (job_work_from_home = TRUE OR job_location LIKE '%CA%') AND
     salary_year_avg IS NOT NULL
 ORDER BY
     salary_year_avg DESC
