13834606f28ae2e33e3831a1839d2c297324dec6
m0niquee <moniquenguyen425@gmail.com>
2026-01-31 11:15:08 -0800

    Add: Top 5 demanded skills for Data Analysts

--- project_sql/3_top_demanded_skills.sql
+++ project_sql/3_top_demanded_skills.sql
@@ -7,9 +7,23 @@ QUESTION: What are the most in-demand skills for data analysts?
     providing insights into the most valuable skills for job seekers
 */
 
-SELECT *
+SELECT
+    skills,
+    COUNT(job_skills.job_id) AS demand_count
 FROM
     job_postings_fact AS job_postings
 INNER JOIN skills_job_dim AS job_skills ON job_skills.job_id = job_postings.job_id
 INNER JOIN skills_dim AS skills ON skills.skill_id = job_skills.skill_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_work_from_home = TRUE
+GROUP BY
+    skills
+ORDER BY
+    demand_count DESC
 LIMIT 5;
+
+/*
+Noticed the trend for ALL data analyst jobs were sql -> excel -> python -> tableau -> power bi
+When altering it to also only list remote jobs, the skills had the same trend
+/*
